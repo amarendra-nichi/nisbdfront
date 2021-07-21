ca7117bf35581c27ff7aecbd443af7cd5af7c665
Amarendra Sharma <s.amarendra1995@gmail.com>
2021-07-22 05:13:31 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,26 @@
 <template>
-  <a-layout id="components-layout-demo-custom-trigger">
-    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
-      <div class="logo" />
-      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
-        <a-menu-item key="1">
-          <a-icon type="user" />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="() => (collapsed = !collapsed)"
-        />
-      </a-layout-header>
-      <a-layout-content
-        :style="{
-          margin: '24px 16px',
-          padding: '24px',
-          background: '#fff',
-          minHeight: '280px',
-        }"
-      >
-        Content
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
+
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
-  data() {
-    return {
-      collapsed: false,
-    };
+  name: "App",
+  components: {
+    HelloWorld,
   },
 };
 </script>
+
 <style>
-#components-layout-demo-custom-trigger .trigger {
-  font-size: 18px;
-  line-height: 64px;
-  padding: 0 24px;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-
-#components-layout-demo-custom-trigger .trigger:hover {
-  color: #1890ff;
-}
-
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import { createApp } from 'vue'
-// import Vuesax from 'vuesax'
-  import App from './App.vue'
-
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import About from "./components/About.vue";
+import {createRouter, createWebHashHistory} from "vue-router"
+const routes = [
+    { path: '/', component: HelloWorld },
+    { path: '/about', component: About },
+  ]
+  
+  // 3. Create the router instance and pass the `routes` option
+  // You can pass in additional options here, but let's
+  // keep it simple for now.
+  const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
 const app = createApp(App);
-
-
+app.use(VueAxios,axios)
+app.use(router)
 app.mount('#app');
